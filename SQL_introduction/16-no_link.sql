aa6632b6ff67283760ef14c2587c58c554e3f250
falyseck <e.seck@alustudent.com>
2024-07-19 05:05:45 -0700

    sql introduction

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
---no link 
+-- list all record in the database table 
 SELECT score, name FROM second_table
-WHERE name IS NOT NULL ORDER BY score DESC;
+WHERE name IS NOT NULL 
+ORDER BY score DESC;
