758d6aaff304ef7a73b86be677071043317aacd0
falyseck <e.seck@alustudent.com>
2024-07-20 04:37:27 -0700

    more queries

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,11 +1,7 @@
--- creates a table and then inserts things in it 
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-INSERT INTO second_table(id, name, score)
-VALUES  (1, 'John', 10),
-        (2, 'Alex', 3),
-        (3, 'Bob', 14),
-        (4, 'George', 8);
+-- Lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
