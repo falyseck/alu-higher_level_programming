5129e512a78a6c6ce65558ff6c1bb2ffc41c1033
falyseck <169052940+falyseck@users.noreply.github.com>
2024-07-20 15:01:02 +0200

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,15 +1,3 @@
--- create_user_and_list_privileges.sql
-
--- Create the user only if it does not already exist
+-- Write a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
-
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply the changes
-FLUSH PRIVILEGES;
-
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
