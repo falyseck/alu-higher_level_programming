d60372f536846837b140334c401569e4977eb452
falyseck <e.seck@alustudent.com>
2024-07-20 03:54:09 -0700

    more queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- 0-privileges.sql
+
+-- Check and show grants for user_0d_1
+SELECT 'user_0d_1' AS 'User', 
+       IF(COUNT(*) > 0, CONCAT('SHOW GRANTS FOR ''user_0d_1''@''localhost'''), 'User does not exist') AS 'Query'
+FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+
+-- Check and show grants for user_0d_2
+SELECT 'user_0d_2' AS 'User', 
+       IF(COUNT(*) > 0, CONCAT('SHOW GRANTS FOR ''user_0d_2''@''localhost'''), 'User does not exist') AS 'Query'
+FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
+
