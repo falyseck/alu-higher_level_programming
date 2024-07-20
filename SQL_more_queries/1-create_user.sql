310d34b996082a69e700fa90699eed67228f30ef
falyseck <169052940+falyseck@users.noreply.github.com>
2024-07-20 14:58:13 +0200

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,8 +3,6 @@
 -- Create the user only if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Ensure the user has the correct password
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
