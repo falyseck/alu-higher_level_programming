c64930af507514545c19afa168df07d42e646397
falyseck <169052940+falyseck@users.noreply.github.com>
2024-08-05 13:48:33 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,5 @@
-ummary__
+#!/usr/bin/python3
+"""__summary__
 - Write a Python script that takes in a URL,
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
