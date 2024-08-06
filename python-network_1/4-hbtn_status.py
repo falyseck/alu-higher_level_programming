d50dad3b11af8cf5fea4c470b63e722f6116097b
faly <e.seck@alustudent.com>
2024-08-06 13:02:57 +0000

    network 1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))    
+    print("\t- content: {}".format(res.text))
