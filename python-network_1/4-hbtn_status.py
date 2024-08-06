a572c53285253ec6b3cd88b0b239b1b978ef1a2a
faly <e.seck@alustudent.com>
2024-08-06 12:45:17 +0000

    network 1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
-"""
+"""Fetch a url"""
+
 import requests
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))    
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))    
